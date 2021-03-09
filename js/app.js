c141684ab81c036625e25bd448060b0d96085cfc
s1azuan <shahrinazuan87@gmail.com>
2021-03-09 17:11:22 +0800

    Change size image

--- js/app.js
+++ js/app.js
@@ -5,3 +5,12 @@ function listTagLink() {
     let navigationLinks = document.getElementsByTagName("nav");
     console.log(navigationLinks);
 }
+
+function changeToDarkMode() {
+    let myElement = document.querySelector("");
+    myElement.style.backgroundColor = "#D93600";
+}
+
+function alert1() {
+    alert("Hi My name is Shahrin Azuan");
+}
